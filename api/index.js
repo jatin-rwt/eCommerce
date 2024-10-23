6f7bb4acc4db43d4fb21f8e4bb779c3ea04be564
jatin-rwt <rawatjatin0206@gmail.com>
2024-10-23 15:15:36 +0530

    setup

--- api/index.js
+++ api/index.js
@@ -1,1 +1,18 @@
-console.log("hi");
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+
+app.get(("/"), (req, res) => {
+    res.send("app is running");
+})
+
+
+
+
+app.listen(PORT || 9000, () => {
+    console.log(`server started on port: ${PORT}`);
+})
