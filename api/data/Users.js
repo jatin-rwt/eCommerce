8a41fbda3cbefbc86537f855d2328afab9629825
jatin-rwt <rawatjatin0206@gmail.com>
2024-10-24 13:17:18 +0530

    Connect to mongodb, creating db models, data seeding

---
+++ api/data/Users.js
@@ -0,0 +1,20 @@
+const bcrypt = require("bcryptjs");
+
+const users = [
+    {
+        name : "Admin",
+        email: "admin@node.com",
+        password: bcrypt.hashSync("123456", 10),
+        isAdmin: true,
+    },
+    {
+        name : "User",
+        email: "user@node.com",
+        password: bcrypt.hashSync("123456", 10),
+        isAdmin: true,
+    },
+
+]
+
+
+module.exports = users;
--- api/index.js
+++ api/index.js
@@ -5,14 +5,32 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 
-
-app.get(("/"), (req, res) => {
-    res.send("app is running");
+const mongoose = require("mongoose");
+//connect to db
+mongoose.connect(process.env.MONGOOSE_URL).then(() => {
+    console.log("db connected");
+}).then((err) => {
+   err;
+    
 })
 
 
+
+
+const databaseSeeder = require("./databaseSeeder");
+
+//database seeder routes
+app.use("/api/seed", databaseSeeder)
 
 
 app.listen(PORT || 9000, () => {
     console.log(`server started on port: ${PORT}`);
 })
+
+
+
+
+
+//rawatjatin0206
+//7u98yI0rDwsQuOeo
+//mongodb+srv://rawatjatin0206:<db_password>@cluster0.0bakv.mongodb.net/
