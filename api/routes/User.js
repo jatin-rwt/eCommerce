8fd733e714f09c17c95a31bf4de417e15fc93d85
jatin-rwt <rawatjatin0206@gmail.com>
2024-10-25 13:05:04 +0530

    user registration with encryption

--- api/routes/User.js
+++ api/routes/User.js
@@ -25,6 +25,38 @@ userRoute.post("/login", AsyncHandler(
     }
 ))
 
+//register route
+userRoute.post("/register", AsyncHandler( async(req, res) => {
+    const {name, email, password} = req.body;
+    const existUser = await User.findOne({email});
+    if(existUser) {
+        res.status(400);
+        throw new Error("User already exists")
+    } else {
+        const user = await User.create({
+            name,
+            email,
+            password
+        })
+
+        if(user) {
+            res.status(201).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                createAt: user.createdAt,
+            })
+        } else{
+            res.status(400);
+            throw new Error("Invalid User Data");
+            
+        }
+    }
+}))
+
+
+
 
 module.exports = userRoute;
 
