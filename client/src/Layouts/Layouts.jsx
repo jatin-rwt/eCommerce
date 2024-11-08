a9dca0cd8e79a38c130c7e6a2159220cd215cdf1
jatin-rwt <rawatjatin0206@gmail.com>
2024-11-08 12:00:12 +0530

    home page layout

---
+++ client/src/Layouts/Layouts.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./NavBar";
+import Footer from "./Footer";
+
+
+
+const Layout = ({children}) =>{
+    return (
+        <>
+        <Navbar/>
+        <main>{children}</main>
+        <Footer/>
+        </>
+    )
+}
+
+export default Layout;
